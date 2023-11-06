5ae74d15b65bc6761acbcf1ce5c41e9a14ee35a9
leulprogramming <63164236+LeulProgramming@users.noreply.github.com>
2023-11-05 23:32:28 -0600

    Initial commit

---
+++ src/secret.py
@@ -0,0 +1,8 @@
+# For the YouTube Data API
+PLAYLIST_ID = "PLqqrBkAAeqhNGlRf3x2Cp1qDO1Q1MngZK"
+
+# For the Spotify Web API
+CLIENT_ID = "530009897664-lltsvf61hl6a9rmmlctk1e9ioo1fp3b3.apps.googleusercontent.com"
+CLIENT_SECRET = "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"
+USER_ID = "92cx6py9dcts8ljqxh4i175ne"
+REDIRECT_URI = "https://www.google.com/"
